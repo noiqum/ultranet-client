6a2e21786debc83ebeab2efbb0e56b127ed562ce
unknown <onurcoskun99@gmail.com>
2020-12-20 01:16:46 +0300

    register test render bug

--- src/pages/register/register.test.tsx
+++ src/pages/register/register.test.tsx
@@ -9,11 +9,11 @@ import Register from "./register";
 afterEach(cleanup);
 function setupWithoutHistory() {
   render(
-    <ApolloProvider client={client}>
-      <MemoryRouter>
+    <MemoryRouter>
+      <ApolloProvider client={client}>
         <Register />
-      </MemoryRouter>
-    </ApolloProvider>
+      </ApolloProvider>
+    </MemoryRouter>
   );
 }
 
@@ -29,15 +29,18 @@ describe("register page", () => {
     expect(usernameInput).toBeInTheDocument();
   });
   test("register form has email field", () => {
+    setupWithoutHistory();
     const emailInput = screen.getByLabelText(/email/i);
     expect(emailInput).toBeInTheDocument();
     screen.debug();
   });
   test("register form has field for password", () => {
+    setupWithoutHistory();
     const passwordInput = screen.getByLabelText(/password/i);
     expect(passwordInput).toBeInTheDocument();
   });
   test("register form has register button", () => {
+    setupWithoutHistory();
     const registerBtn = screen.getByRole("button", { name: /register/i });
     expect(registerBtn).toBeInTheDocument();
   });
