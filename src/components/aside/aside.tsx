f9c483a38e1a9a17132355ab4b0677a368ed1887
unknown <onurcoskun99@gmail.com>
2020-12-22 00:26:06 +0300

    aside component mobile version

--- src/components/aside/aside.tsx
+++ src/components/aside/aside.tsx
@@ -11,58 +11,172 @@ import { ReactComponent as Dance } from "../../svg/dance.svg";
 import { ReactComponent as Animals } from "../../svg/animals.svg";
 import { ReactComponent as Motorbike } from "../../svg/motorbike.svg";
 import { ReactComponent as Nature } from "../../svg/nature.svg";
+import { ReactComponent as Hamburger } from "../../svg/hamburger.svg";
+import { ReactComponent as Arrow } from "../../svg/arrow.svg";
 
 const Aside: React.FC = () => {
+  const [active, setActive] = React.useState<boolean>(false);
+
   return (
-    <div className="aside">
-      <div className="aside__logo">Ultranet</div>
-      <div className="aside__menu">
-        <div className="aside__menu__title">Menu</div>
-        <div className="aside__menu__list">
-          <div className="aside__menu__list-item">
-            <Home /> <span>Home</span>
+    <>
+      <div className={active ? "aside-active" : "aside"}>
+        <div
+          className={active ? "aside-active__button" : "aside__button"}
+          onClick={() => {
+            setActive(!active);
+          }}
+        >
+          {active ? <Arrow /> : <Hamburger />}
+        </div>
+        <div className={active ? "aside-active__logo" : "aside__logo"}>
+          Ultranet
+        </div>
+        <div className={active ? "aside-active__menu" : "aside__menu"}>
+          <div
+            className={
+              active ? "aside-active__menu__title" : "aside__menu__title"
+            }
+          >
+            Menu
           </div>
-          <div className="aside__menu__list-item">
-            <News /> <span>latest news</span>
+          <div
+            className={
+              active ? "aside-active__menu__list" : "aside__menu__list"
+            }
+          >
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Home /> <span>Home</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <News /> <span>latest news</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Map /> <span>explore</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Files /> <span>files</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Gallery /> <span>gallery</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Events /> <span>events</span>
+            </div>
           </div>
-          <div className="aside__menu__list-item">
-            <Map /> <span>explore</span>
+        </div>
+        <div className={active ? "aside-active__menu" : "aside__menu"}>
+          <div
+            className={
+              active ? "aside-active__menu__title" : "aside__menu__title"
+            }
+          >
+            your favourite
           </div>
-          <div className="aside__menu__list-item">
-            <Files /> <span>files</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Gallery /> <span>gallery</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Events /> <span>events</span>
+          <div
+            className={
+              active ? "aside-active__menu__list" : "aside__menu__list"
+            }
+          >
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Forest /> <span>Forest</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Birds /> <span>birds</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Nature /> <span>nature</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Animals /> <span>animals</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Motorbike /> <span>motorbike</span>
+            </div>
+            <div
+              className={
+                active
+                  ? "aside-active__menu__list-item"
+                  : "aside__menu__list-item"
+              }
+            >
+              <Dance /> <span>dance</span>
+            </div>
           </div>
         </div>
       </div>
-      <div className="aside__menu">
-        <div className="aside__menu__title">your favourite</div>
-        <div className="aside__menu__list">
-          <div className="aside__menu__list-item">
-            <Forest /> <span>Forest</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Birds /> <span>birds</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Nature /> <span>nature</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Animals /> <span>animals</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Motorbike /> <span>motorbike</span>
-          </div>
-          <div className="aside__menu__list-item">
-            <Dance /> <span>dance</span>
-          </div>
-        </div>
-      </div>
-    </div>
+      <div
+        className={active ? "overlay-active" : "overlay"}
+        onClick={() => {
+          setActive(!active);
+        }}
+      ></div>
+    </>
   );
 };
 
